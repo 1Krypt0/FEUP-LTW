bd7c5ebef4d9dcff9e1fa42a19e27d6659724235
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2021-12-15 23:52:36 +0000

    Adds special file for header

---
+++ Project/js/header.js
@@ -0,0 +1,17 @@
+class Header {
+    show() {
+        return `<h1> Mancala: The best game you never heard of</h1>
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">Logout</a>
+                </li>
+            </ul>
+        </nav>`;
+    }
+}
+
+const main = document.getElementById("main");
+const header = document.createElement("header");
+header.innerHTML = new Header().show();
+main.appendChild(header);
