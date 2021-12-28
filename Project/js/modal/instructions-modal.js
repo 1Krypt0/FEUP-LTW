056fa880596579704efda00ac3fbb90bb1c78305
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2021-12-28 17:47:08 +0000

    Change name of id to lowercase

--- Project/js/modal/instructions-modal.js
+++ Project/js/modal/instructions-modal.js
@@ -1,7 +1,7 @@
 // Get the modal
 let instructionsModal = document.getElementById("instructions-modal");
 
-let instructionsButton = document.getElementById("Instructions");
+let instructionsButton = document.getElementById("instructions");
 
 // Get the <span> element that closes the modal
 let instructionsCloser = document.getElementById("instructions-modal-close");
