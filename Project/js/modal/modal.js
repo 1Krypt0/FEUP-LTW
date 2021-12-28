f21b5652e72a484fed76dfcd900243df9a101d46
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2021-12-28 15:45:28 +0000

    Change modal definitions

--- Project/js/modal/modal.js
+++ Project/js/modal/modal.js
@@ -1,15 +1,15 @@
 // Get the modal
-var modal = document.getElementById("myModal");
+let modal = document.getElementById("myModal");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+let span = document.getElementById("modal-close");
 
-// When the user clicks on the button, open the modal
+console.log(span);
+
 window.onload = function () {
     modal.style.display = "block";
 };
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function () {
     modal.style.display = "none";
 };
