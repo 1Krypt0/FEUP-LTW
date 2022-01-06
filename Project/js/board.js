3736ad189e22b556b67cdb25c598d4cc15ee2855
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2022-01-06 16:10:44 +0000

    Add prototype for keeping track of pit and store amounts

--- Project/js/board.js
+++ Project/js/board.js
@@ -15,13 +15,25 @@ export class Board {
         let rowTwo = document.querySelector(".row.player-two");
 
         for (let i = 0; i < this.getSize(); i++) {
+            let pitAndCounter1 = document.createElement("div");
+            let pitAndCounter2 = document.createElement("div");
             let pitPlayer1 = document.createElement("div");
             let pitPlayer2 = document.createElement("div");
+            let pitTracker1 = document.createElement("div");
+            let pitTracker2 = document.createElement("div");
+            pitAndCounter1.classList.add("tracker");
+            pitAndCounter2.classList.add("tracker");
             pitPlayer1.classList.add("pit");
             pitPlayer2.classList.add("pit");
 
-            rowOne.appendChild(pitPlayer1);
-            rowTwo.appendChild(pitPlayer2);
+            pitAndCounter1.appendChild(pitTracker1);
+            pitAndCounter1.appendChild(pitPlayer1);
+
+            pitAndCounter2.appendChild(pitTracker2);
+            pitAndCounter2.appendChild(pitPlayer2);
+
+            rowOne.appendChild(pitAndCounter1);
+            rowTwo.appendChild(pitAndCounter2);
         }
     }
 }
