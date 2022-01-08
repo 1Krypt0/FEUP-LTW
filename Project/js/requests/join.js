06af957bb7b20e13425de6e8da84406cb6160b02
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2022-01-08 11:26:53 +0000

    Implement join game request

--- Project/js/requests/join.js
+++ Project/js/requests/join.js
@@ -1,9 +1,15 @@
 import { MyRequest } from "./requests.js";
-const
+import { nick, pass } from "./auth/login.js";
+import { Board } from "../board.js";
+import { playGame } from "../mvc/script.js";
 const GROUP_ID = 44;
+let GAME_ID = undefined;
+
+const paramsButton = document.getElementById("parameters");
+const paramsModal = document.getElementById("myModal");
 
 function join() {
-    const request = new 
+    const request = new MyRequest("POST", "join", getParams());
 
     let response = request.sendRequest();
 
@@ -12,13 +18,37 @@ function join() {
     });
 }
 
-function processJoin(result) {}
+function processJoin(result) {
+    if (result.game === undefined) {
+    } else {
+        GAME_ID = result.game;
+        console.log(getParams());
+        setGame(getParams().initial, getParams().size);
+        paramsModal.style.display = "none";
+    }
+}
 
 function getParams() {
     const sizeInput = document.getElementById("pitNo");
     const seedInput = document.getElementById("seedNo");
-    const size = sizeInput.value;
-    const initial = seedInput.value;
+    const size = sizeInput.value === "" ? 6 : sizeInput.value;
+    const initial = seedInput.value === "" ? 4 : sizeInput.value;
 
-    
+    let data = {
+        grop: GROUP_ID,
+        nick: nick,
+        password: pass,
+        size: size,
+        initial: initial,
+    };
+
+    return data;
 }
+
+function setGame(seedAmount, pitAmount) {
+    let board = new Board(pitAmount, seedAmount);
+    board.render();
+    playGame(pitAmount, seedAmount);
+}
+
+paramsButton.onclick = join;
