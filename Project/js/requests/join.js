f16f2e2ef646a211735156579c964a04a985bf1b
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2022-01-08 09:32:37 +0000

    Move requests to appropriate folder. Create join prototype

---
+++ Project/js/requests/join.js
@@ -0,0 +1,10 @@
+const requests = require("./requests.js");
+const GROUP_ID = 44;
+
+function join() {
+    const sizeInput = document.getElementById("pitNo");
+    const seedInput = document.getElementById("seedNo");
+
+    const size = sizeInput.value;
+    const initial = seedInput.value;
+}
