0dc5fb62637a8542481fc7040bed981b9a3bf78a
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2022-01-08 09:47:21 +0000

    Develop join even more

--- Project/js/requests/join.js
+++ Project/js/requests/join.js
@@ -1,10 +1,24 @@
-const requests = require("./requests.js");
+import { MyRequest } from "./requests.js";
+const
 const GROUP_ID = 44;
 
 function join() {
+    const request = new 
+
+    let response = request.sendRequest();
+
+    response.then(function (result) {
+        processJoin(result);
+    });
+}
+
+function processJoin(result) {}
+
+function getParams() {
     const sizeInput = document.getElementById("pitNo");
     const seedInput = document.getElementById("seedNo");
-
     const size = sizeInput.value;
     const initial = seedInput.value;
+
+    
 }
