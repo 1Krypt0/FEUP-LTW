03c4ff877750df3645ee0f8ddf4fd95415fd5937
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2022-01-16 16:41:54 +0000

    Rewrites the files with Prettier

--- Project/js/mvc/script.js
+++ Project/js/mvc/script.js
@@ -3,61 +3,58 @@ import { MancalaView } from "./view.js";
 import { Mancala } from "./model.js";
 
 export class Game {
-    constructor(size, seeds) {
-        this.model_ = new Mancala(size, seeds);
-        this.view_ = new MancalaView(this.model_);
-        this.controller_ = new MancalaController(this.model_, this.view_);
-    }
+  constructor(size, seeds) {
+    this.model_ = new Mancala(size, seeds);
+    this.view_ = new MancalaView(this.model_);
+    this.controller_ = new MancalaController(this.model_, this.view_);
+  }
 
-    getModel() {
-        return this.model_;
-    }
+  getModel() {
+    return this.model_;
+  }
 
-    getView() {
-        return this.view_;
-    }
+  getView() {
+    return this.view_;
+  }
 
-    getController() {
-        return this.controller_;
-    }
+  getController() {
+    return this.controller_;
+  }
 
-    init() {
-        this.getView().render();
-    }
+  init() {
+    this.getView().render();
+  }
 }
 
 export function playGame(size, seeds) {
-    let game = new Game(size, seeds);
-    let waitingForMove = true;
+  let game = new Game(size, seeds);
+  let waitingForMove = true;
 
-    game.init();
+  game.init();
 
-    let bindPitsAction = function (player, row) {
-        let clickAction = function () {
-            if (
-                game.getModel().getCurrentPlayer() === player &&
-                waitingForMove
-            ) {
-                waitingForMove = false;
-                let pit = this;
-                if (!game.getController().doPlayerTurn(pit)) {
-                    waitingForMove = true;
-                }
-            }
-        };
-
-        for (let pit = 0; pit < row.length; pit++) {
-            row[pit].setAttribute("data-pit", pit);
-            row[pit].onclick = clickAction;
+  let bindPitsAction = function (player, row) {
+    let clickAction = function () {
+      if (game.getModel().getCurrentPlayer() === player && waitingForMove) {
+        waitingForMove = false;
+        let pit = this;
+        if (!game.getController().doPlayerTurn(pit)) {
+          waitingForMove = true;
         }
+      }
     };
 
-    bindPitsAction(
-        "one",
-        document.querySelectorAll(".row.player-one .pitAndTracker")
-    );
-    bindPitsAction(
-        "two",
-        document.querySelectorAll(".row.player-two .pitAndTracker")
-    );
+    for (let pit = 0; pit < row.length; pit++) {
+      row[pit].setAttribute("data-pit", pit);
+      row[pit].onclick = clickAction;
+    }
+  };
+
+  bindPitsAction(
+    "one",
+    document.querySelectorAll(".row.player-one .pitAndTracker")
+  );
+  bindPitsAction(
+    "two",
+    document.querySelectorAll(".row.player-two .pitAndTracker")
+  );
 }
--- Project/js/requests/requests.js
+++ Project/js/requests/requests.js
@@ -1,37 +1,37 @@
 const URL = "http://twserver.alunos.dcc.fc.up.pt:8008/";
 
 class MyRequest {
-    constructor(method, url, obj) {
-        this.method_ = method;
-        this.url_ = url;
-        this.obj_ = obj;
-    }
+  constructor(method, url, obj) {
+    this.method_ = method;
+    this.url_ = url;
+    this.obj_ = obj;
+  }
 
-    async sendRequest() {
-        if (this.method_ === "GET") {
-            try {
-                const response_1 = await fetch(URL + this.url_);
-                const data = await response_1.json();
-                return data;
-            } catch (error) {
-                console.log("Error:", error);
-            }
-        } else if (this.method_ === "POST") {
-            try {
-                const response_1 = await fetch(URL + this.url_, {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify(this.obj_),
-                });
-                const data = await response_1.json();
-                return data;
-            } catch (error) {
-                console.error("Error:", error);
-            }
-        }
+  async sendRequest() {
+    if (this.method_ === "GET") {
+      try {
+        const response_1 = await fetch(URL + this.url_);
+        const data = await response_1.json();
+        return data;
+      } catch (error) {
+        console.log("Error:", error);
+      }
+    } else if (this.method_ === "POST") {
+      try {
+        const response_1 = await fetch(URL + this.url_, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(this.obj_),
+        });
+        const data = await response_1.json();
+        return data;
+      } catch (error) {
+        console.error("Error:", error);
+      }
     }
+  }
 }
 
 export { MyRequest };
