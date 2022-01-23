42d895a689e9c0deb60ead639ca2d91f7d089dd4
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2022-01-23 22:17:29 +0000

    Add checks to enforce correct arguments

--- Project/js/server/handlers/notify.js
+++ Project/js/server/handlers/notify.js
@@ -1,3 +1,5 @@
+const game = require("./join");
+
 exports.handleNotify = function (body) {
   const data = JSON.parse(body);
   console.log(data);
@@ -11,4 +13,26 @@ exports.handleNotify = function (body) {
   } else if (data.move === undefined) {
     return [{ error: "Move not defined" }, 400];
   }
+
+  console.log(game.GAME);
+
+  if (game.GAME === null) {
+    return [{ error: "No game with ID of " + data.game }, 400];
+  }
+
+  if (game.GAME.getModel().getCurrentPlayer() !== data.nick) {
+    return [{ error: "Not your turn to play" }, 400];
+  }
+
+  if (isNaN(parseInt(data.move))) {
+    return [{ error: "Invalid move" }, 400];
+  }
+
+  const move = parseInt(data.move);
+
+  if (move < 0 || move >= game.GAME.getModel().getPlayer2StoreIdx()) {
+    return [{ error: "Invalid move" }, 400];
+  }
 };
+
+function makeMove(game, move) {}
