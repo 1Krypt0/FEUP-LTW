1adba5aae9fd244abcbae61aca2e37849814d357
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2022-01-23 15:18:44 +0000

    Add create game hash part

--- Project/js/server/handlers/join.js
+++ Project/js/server/handlers/join.js
@@ -2,19 +2,47 @@ const fs = require("fs");
 const crypto = require("crypto");
 
 const GAME_STARTED = null;
+let PLAYER_1 = null;
+let PLAYER_2 = null;
 
 exports.handleJoin = function (body) {
-  if (body.group === undefined) {
+  const data = JSON.parse(body);
+
+  if (data.group === undefined) {
+    console.log("Group not defined");
     return [{ error: "Group not defined" }, 400];
-  } else if (body.nick === undefined) {
+  } else if (data.nick === undefined) {
+    console.log("User not defined");
     return [{ error: "User not defined" }, 400];
-  } else if (body.password === undefined) {
+  } else if (data.password === undefined) {
+    console.log("Password not defined");
     return [{ error: "Password not defined" }, 400];
-  } else if (body.initial === undefined) {
+  } else if (data.initial === undefined) {
+    console.log("Initial seed amount not defined");
     return [{ error: "Initial seed amount not defined" }, 400];
-  } else if (body.size === undefined) {
+  } else if (data.size === undefined) {
+    console.log("Board size not defined");
     return [{ error: "Board size not defined" }, 400];
   }
+
+  const currentDate = new Date();
+  const time =
+    currentDate.getFullYear().toString() +
+    (currentDate.getMonth() + 1).toString() +
+    currentDate.getDate().toString() +
+    currentDate.getHours().toString() +
+    currentDate.getMinutes().toString();
+
+  const GAME_DATA =
+    data.group.toString() +
+    data.nick +
+    data.password +
+    data.initial.toString() +
+    data.size.toString() +
+    time;
+
+  const GAME_HASH = crypto.createHash("md5").update(GAME_DATA).digest("hex");
+  console.log(GAME_HASH);
 
   return [{}, 200];
 };
