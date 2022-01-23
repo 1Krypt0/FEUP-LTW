341f551ecb0ccca5a78ebfaa6276447bc61c496b
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2022-01-23 10:43:44 +0000

    Add Ranking modal to main page

--- Project/js/modal/scores-modal.js
+++ Project/js/modal/scores-modal.js
@@ -4,12 +4,12 @@ let scoresModal = document.getElementById("scores-modal");
 let scoresButton = document.getElementById("scores");
 
 // Get the <span> element that closes the modal
-let instructionsCloserr = document.getElementById("scores-modal-close");
+let scoresCloser = document.getElementById("scores-modal-close");
 
 scoresButton.onclick = function () {
   scoresModal.style.display = "block";
 };
 
-instructionsCloserr.onclick = function () {
+scoresCloser.onclick = function () {
   scoresModal.style.display = "none";
 };
