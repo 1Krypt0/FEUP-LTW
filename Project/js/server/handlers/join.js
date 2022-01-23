7dc71ea9fb6a2c025df93484ce52711276ca9159
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2022-01-23 15:01:13 +0000

    Add ranking stub and join request validation

--- Project/js/server/handlers/join.js
+++ Project/js/server/handlers/join.js
@@ -4,6 +4,17 @@ const crypto = require("crypto");
 const GAME_STARTED = null;
 
 exports.handleJoin = function (body) {
-  console.log(body);
+  if (body.group === undefined) {
+    return [{ error: "Group not defined" }, 400];
+  } else if (body.nick === undefined) {
+    return [{ error: "User not defined" }, 400];
+  } else if (body.password === undefined) {
+    return [{ error: "Password not defined" }, 400];
+  } else if (body.initial === undefined) {
+    return [{ error: "Initial seed amount not defined" }, 400];
+  } else if (body.size === undefined) {
+    return [{ error: "Board size not defined" }, 400];
+  }
+
   return [{}, 200];
 };
